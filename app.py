701afabfc08463c314aeb547bdf71dd1ae922f40
Pinpinut <pinpinut@Pinpinuts-MacBook-Air.local>
2022-12-08 10:38:20 +0700

    get current time and room number

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') 
 def index():
+    return render_template('index.html')
+
+@app.route('/start/<room>')
+def start(room):
+    current_time = datetime.now().strftime("%H:%M:%S")
+    print(f"room: {room}, time: {current_time}")
     return render_template('index.html')
 
 if __name__ == "__main__":
